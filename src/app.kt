497a5f1e3819d87c375b244dda08733d0731cc93
Ramadani <dani@javan.co.id>
2017-05-23 16:24:21 +0700

    first commit

---
+++ src/app.kt
@@ -0,0 +1,10 @@
+import example.Person
+
+/**
+ * Created by dani on 5/23/17.
+ */
+
+fun main(args: Array<String>) {
+    val person = Person("Gejrot")
+    person.greet()
+}
---
+++ src/example/Person.kt
@@ -0,0 +1,10 @@
+package example
+
+/**
+ * Created by dani on 5/23/17.
+ */
+class Person(val firstName: String) {
+    fun greet() {
+        print(firstName)
+    }
+}
